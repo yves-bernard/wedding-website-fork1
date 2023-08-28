ed7a8386c65c52da7751f72d7503cc755c629da0
Yves Bernard <yves.bernard1804@gmail.com>
2023-08-28 23:48:44 +0200

    Fixing incorrect language
    Signed-off-by: Yves Bernard <yves.bernard1804@gmail.com>

--- languages.js
+++ languages.js
@@ -25,16 +25,16 @@ var data = {
           label:"Francais",
           title: "Le mariage de Deborah et Yves",
           description: "Bienvenue sur le site de notre mariage.",
-          first_met: "When we first met.",
-          header: "When we first met.",
-          date: "Le mariage aura lieu le samedi 4 mai 2024 au moulin de nartaud."
+          first_met: "Quand on s'est rencontre.",
+          header: "Comment s'est on rencontre.",
+          date: "Wedding will be on the saturday 4th of may at the moulin de nartaud."
      },
      english: {
           label:"English",
           title: "Deb and Yves's wedding",
           description: "Welcome to our wedding website.",
-          first_met: "Quand on s'est rencontre.",
-          header: "Comment s'est on rencontre.",
-          date: "Wedding will be on the saturday 4th of may at the moulin de nartaud."
+          first_met: "Deborah and I met in Norway during erasmus exactly 10 years and we've never left each other since.We're delighted to invite our friends and family to celebrate our wedding on the 04/05/2025 ! It will be great craic.",
+          header: "When we first met.",
+          date: "Le mariage aura lieu le samedi 4 mai 2024 au moulin de nartaud."
      },
 }
