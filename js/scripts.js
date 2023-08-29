f4fd28cc47e7e2f4785b1685fea306c9cf1f3acb
Yves Bernard <yves.bernard1804@gmail.com>
2023-08-29 23:11:06 +0200

    Updating rsvp form dependencies
    Signed-off-by: Yves Bernard <yves.bernard1804@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -214,11 +214,11 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+        if (MD5($('#invite_code').val()) !== 'aa2f5575f57fddf3408435af3f7b886b'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxFjlev6cmPOhycwGolWD78b9_XB064m27esqO6hpsDKnZH8bqthNDxBEdSTxW9_9wS/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
