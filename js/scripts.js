11ac0632da8a2138d059947d1e2e38154cd01085
Yves Bernard <yves.bernard1804@gmail.com>
2023-08-29 23:55:16 +0200

    Updating calendar
    Signed-off-by: Yves Bernard <yves.bernard1804@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Deb and Yves's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('May 04, 2024 14:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('May 05, 2024 18:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Le Moulin de Nartaud, Toulouse, France',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any question you can reach us at debandyves@gmail.com."
         }
     });
 
